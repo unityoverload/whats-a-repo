e719a6184c3deb9a27abdabb459304b92c62910e
AdamGrealish <agrealish@gmail.com>
2016-12-14 08:10:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
     //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
-    });
-    
-    
     
     
 });
